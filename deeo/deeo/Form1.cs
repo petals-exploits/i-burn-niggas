0993f4f6e131c87c71f3d95023c524c83bf8261d
petals-exploits <60903136+petals-exploits@users.noreply.github.com>
2020-04-10 20:49:52 -0400

    Add files via upload

---
+++ deeo/deeo/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyExploits;
+
+namespace deeo
+{
+    public partial class Form1 : Form
+    {
+        EasyExploits.Module module = new EasyExploits.Module();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { }
+            public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void FormMoveable_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            Random rand = new Random();
+            int A = rand.Next(0, 255);
+            int R = rand.Next(0, 255);
+            int G = rand.Next(0, 255);
+            int B = rand.Next(0, 255); label2.ForeColor = Color.FromArgb(A, R, G, B);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            module.ExecuteScript(richTextBox1.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //wassup
+            OpenFileDialog opendialogfile = new OpenFileDialog();
+            opendialogfile.Filter = "Lua File (*.lua)|*.lua|Text File (*.txt)|*.txt";
+            opendialogfile.FilterIndex = 2;
+            opendialogfile.RestoreDirectory = true;
+            if (opendialogfile.ShowDialog() != DialogResult.OK)
+                return;
+            try
+            {
+                richTextBox1.Text = "";
+                System.IO.Stream stream;
+                if ((stream = opendialogfile.OpenFile()) == null)
+                    return;
+                using (stream)
+                    this.richTextBox1.Text = System.IO.File.ReadAllText(opendialogfile.FileName);
+            }
+            catch (Exception)
+            {
+                int num = (int)MessageBox.Show("An unexpected error has occured", "OOF!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            module.LaunchExploit();
+        }
+    }
+    }
